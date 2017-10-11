77ead57fb86cc1fbcde061894a0f4c26ae0bce13
magnybor <magnybor@gmail.com>
2017-10-11 09:25:46 +0200

    creation des 2 premiers écrans

--- app/src/main/java/fr/greyl/robin/applicationtest/MainActivity.java
+++ app/src/main/java/fr/greyl/robin/applicationtest/MainActivity.java
@@ -2,12 +2,66 @@ package fr.greyl.robin.applicationtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button b_online = (Button) findViewById(R.id.b_vsPonline);
+
+        b_online.setOnClickListener(new View.OnClickListener() {
+            int i = 1;
+            @Override
+            public void onClick(View v) {
+                //while(true) {
+                    b_online.setText(String.valueOf(i++));
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+               // }
+
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
 }
